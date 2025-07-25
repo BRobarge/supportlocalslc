0aa24bd095db3dfd6d6c1687b66b025f14678661
BRobarge <bobbyrobarge@gmail.com>
2025-07-24 20:26:58 -0600

    testing

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
-export const BASE_PATH = '/sandy'; // Set to '/sandy' for now; change to '/draper' or '' for other cities
-export const SITE_DOMAIN = 'supportlocalslc.com'; // Updated to your custom domain
+export const BASE_PATH = '/sandy';
+export const SITE_DOMAIN = 'supportlocalslc.com';
 export const getBusinessUrl = (id) => `${BASE_PATH}/business.html?id=${id}`;
 export const getDashboardUrl = () => `${BASE_PATH}/dashboard.html`;
 export const getLoginUrl = () => `${BASE_PATH}/login.html`;
